fefeb3e92f0a38dc8d8eea78e1cd238fac04792f
bdavenport <the.lost.minded@gmail.com>
2013-07-03 16:26:48 -0400

    Added the planes class, more work to be done here.

--- base.py
+++ base.py
@@ -1,5 +1,10 @@
-import re, os, webapp2
+import datetime
+import re
+import os
+import logging
+import webapp2
 
+from google.appengine.api import memcache
 from webapp2_extras import jinja2
 
 # Handles a bit more than standard webapp2 server
@@ -75,7 +80,7 @@ class Plane():
         self.isOnGround = False
 
         # Last time we updated any data
-        self.lastupdate = datetime.utcnow()
+        self.lastupdate = datetime.datetime.utcnow()
 
         # Is this plane in the KML/JSON?
         self.showKML = True
@@ -83,4 +88,73 @@ class Plane():
 # Lets group the planes together
 class Planes():
     def __init__(self):
-        self.planes = []
+        # Our planes
+        self.planes = {}
+
+    def Warmup(self):
+        data = memcache.get(planeicaos)
+        if data is not None:
+            for icao in data:
+                self.planes[icao] = self.pullPlane(icao)
+
+    def newPlane(self, icao):
+        plane = Plane()
+        self.pushPlane(icao, plane)
+        return plane
+
+    def checkLocalPlane(self, icao):
+        if icao in self.planes:
+            return True
+        else:
+            return False
+
+    def checkMemcachePlane(self, icao):
+        data = memcache.get(icao)
+        if data is not None:
+            return True
+        else:
+            return False
+
+    # Update our local array of planes and update memcache for other instances
+    def pushPlane(self, icao, plane):
+        self.planes[icao] = plane
+        memcache.set(key=icao, value=plane, time=900)
+
+    # Grab this plane somehow...
+    def pullPlane(self, icao):
+        # Do we have this plane locally already?
+        if self.checkLocalPlane(icao):
+            localplane = self.planes[icao]
+        else:
+            localplane = None
+
+        # Does the plane exist in memcache?
+        if self.checkMemcachePlane(icao):
+            remoteplane = memcache.get(icao)
+        else:
+            remoteplane = None
+
+        # If we have both local and remote
+        if localplane is not None and remoteplane is not None:
+            # If the local copy is newer, push it
+            if localplane.lastupdate > remoteplane.lastupdate:
+                self.pushPlane(icao, localplane)
+                return localplane
+            else:
+                return remoteplane
+        elif localplane is None and remoteplane is not None:
+            # Add localally and run with it
+            self.pushPlane(icao, remoteplane)
+            return remoteplane
+        elif localplane is not None and remoteplane is None:
+            # Should only happen if the key expires in memcache
+            self.pushPlane(icao, localplane)
+            return localplane
+        else:
+            plane = self.newPlane(icao)
+            return plane
+
+    def processBasestation(self, msgs):
+        for msg in msgs:
+            tmp = msg.split(',')
+            logging.info(tmp)
